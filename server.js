aa2cdce1e9e63b1f5f13671dc1c08023a6a08be2
kirill <kirill.klimuk@gmail.com>
2018-04-19 18:11:32 +0500

    dfasdfasdfsafdasdfs

--- server.js
+++ server.js
@@ -172,9 +172,11 @@ wss.on('connection', function connection(ws) {
         console.log('[WebSocket]', 'received', mess);
     });
     console.log("before query " + user_id);
-    try {
-        const res = await pool.query(`select array_to_json(array_agg(row_to_json(t))) as json from (
-            select id, note, to_char(creation_date,'FMDay, FMDD Mon HH12:MI:SS') as commentDate from "Notes" where user_id = '${user_id}') t`);
+    pool.query(`select array_to_json(array_agg(row_to_json(t))) as json from (
+		select id, note, to_char(creation_date,'FMDay, FMDD Mon HH12:MI:SS') as commentDate from "Notes" where user_id = '${user_id}') t`, (err, res) => {
+            if (err) {
+                console.log('Error executing query');
+            }
             console.log("DADADADA");
             var arr = res.rows[0].json;
             if (IsJsonArray(arr)) {
@@ -186,10 +188,7 @@ wss.on('connection', function connection(ws) {
                 console.log("sended " + JSON.stringify(mess));
                 ws.send(JSON.stringify(mess));
             }
-      } catch(err) {
-        console.log(err.stack)
-      }
-
+        });
         console.log("ZZZZZZZZZZZ");
 });
 server.listen(8081, function listening() {
