7a4fa2c27d93a442f9a56fbbb4bf2bc88dab35e3
kirill <kirill.klimuk@gmail.com>
2018-04-20 19:02:26 +0500

    asddsdas

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 const WS_URI = `ws://${window.location.host}`;
 const ws = new WebSocket(WS_URI);
 
-Vue.use(VueTinySlider);
+
 
 const app = new Vue({
     el: '#app',
@@ -44,7 +44,7 @@ const app = new Vue({
         }
     },
 });
-
+app.use(VueTinySlider);
 ws.onopen = () => {
     console.log('[WebSocket]', 'connected to', WS_URI);
     app.$data.isReady = true;
